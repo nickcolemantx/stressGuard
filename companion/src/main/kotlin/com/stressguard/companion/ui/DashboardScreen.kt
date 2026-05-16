b8632f6ba310dd00ed923765ca14659bda66fa15
Nick <nickcolemantx@gmail.com>
2026-05-15 22:55:14 -0500

    init

---
+++ companion/src/main/kotlin/com/stressguard/companion/ui/DashboardScreen.kt
@@ -0,0 +1,8 @@
+package com.stressguard.companion.ui
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun DashboardScreen() {
+    // TODO: connection status, current stress card, today summary, mini 4H chart, recent alerts list.
+}
